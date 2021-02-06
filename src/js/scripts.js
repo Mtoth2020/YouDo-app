302ec0850ede23b3971335063b4fb9f9562093b0
MELINDA TÓTH <toth.melinda1982@gmail.com>
2021-02-06 10:31:48 +0100

    MZ-01..MZ-03-ig

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,2 +1,28 @@
-//can be deleted
-console.log("Console log works!");
+/*When we open the application, we see an form, where I can type my task.
+When we click on the 'create' button, the typed task item appears on the screen. The screen can contain many tasks.
+An button appears next to every single item. If we click on this button,
+the item is deleted from the "YouDo" list.*/
+const divEl = document.querySelector('div');
+const ulElement = document.createElement("ul");
+const createButton = document.getElementById('createButton');
+createButton.addEventListener('click', newTodo);
+
+function newTodo() {
+    const addNewTodo = document.getElementById('newTodo');
+    const liElement = document.createElement("li");
+    const delButton = document.createElement("button");
+    liElement.innerText = addNewTodo.value;
+    ulElement.prepend(liElement);
+    liElement.appendChild(delButton);
+    delButton.innerText = "delete";
+    divEl.append(ulElement);
+    delButton.addEventListener('click', () => handleDelete(liElement));
+}
+
+function handleDelete(liElement) {
+    liElement.remove();
+}
+
+
+
+
