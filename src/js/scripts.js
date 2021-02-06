8c55c4522b139c3cc4f9edc81e2dc919debb256e
MELINDA TÓTH <toth.melinda1982@gmail.com>
2021-02-06 13:04:21 +0100

    added MZ 04-05-06 tasks

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,13 +1,27 @@
 /*When we open the application, we see an form, where I can type my task.
-When we click on the 'create' button, the typed task item appears on the screen. The screen can contain many tasks.
+When we click on the 'create' button, the typed task item appears on the screen.
+The screen can contain many tasks.
 An button appears next to every single item. If we click on this button,
 the item is deleted from the "YouDo" list.*/
+
 const divEl = document.querySelector('div');
 const ulElement = document.createElement("ul");
 const createButton = document.getElementById('createButton');
-createButton.addEventListener('click', newTodo);
+//let myTodoBasket = [];
 
-function newTodo() {
+isBasketStatus();
+
+function isBasketStatus() {
+    if(localStorage.getItem('savedList') !== "") {
+        divEl.innerHTML = JSON.parse(localStorage.getItem('savedList'));
+    } else {
+        divEl.innerHTML = "";
+    }
+}
+
+createButton.addEventListener('click', createNewTodo);
+
+function createNewTodo() {
     const addNewTodo = document.getElementById('newTodo');
     const liElement = document.createElement("li");
     const delButton = document.createElement("button");
@@ -16,11 +30,47 @@ function newTodo() {
     liElement.appendChild(delButton);
     delButton.innerText = "delete";
     divEl.append(ulElement);
+    //myTodoBasket.push(divEl.innerHTML);
+    //localStorage.setItem('savedList', JSON.stringify(myTodoBasket));
+    localStorage.setItem('savedList', JSON.stringify(divEl.innerHTML));
     delButton.addEventListener('click', () => handleDelete(liElement));
 }
 
 function handleDelete(liElement) {
     liElement.remove();
+}
+
+/*The input field validates the task, it can not be empty and the task description
+must be at least 6 characters long. The validation process runs at every keyup event.
+The input field is red, when the task description doesn't match the validation criterias,
+and green, when it is ready to create.
+ */
+const newTodoInput = document.getElementById('newTodo');
+
+function validateInput() {
+    const input = newTodoInput.value;
+    const isMinSixChar = input.length >= 6;
+    return input !== "" && isMinSixChar;
+}
+
+newTodoInput.addEventListener("keyup", event => {
+    event.preventDefault();
+    if(!validateInput()) {
+        newTodoInput.style.backgroundColor= 'red';
+    } else {
+        newTodoInput.style.backgroundColor= 'greenyellow';
+    }
+    createButtonDisableStatus();
+});
+
+//The create button is disabled so far the input doesn't pass the validation.
+
+function createButtonDisableStatus() {
+    if (validateInput()) {
+        createButton.disabled = false;
+    } else {
+        createButton.disabled = true;
+    }
 }
 
 
